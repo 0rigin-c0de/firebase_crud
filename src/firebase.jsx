ec582e9d426093cb1caaf30b06ce7bff5fa89826
0rigin-c0de <shunnu807@gmail.com>
2024-03-28 22:12:14 +0530

    configure firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.database();
+const auth = app.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export { app, db, auth, googleProvider };
